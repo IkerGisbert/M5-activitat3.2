74a5953f5992c813d0e17391900efa4627162a2c
IkerGisbert <ikergisbert@iesmontsia.org>
2023-02-06 08:59:58 +0100

    COMMIT

--- TreballadorTest.java
+++ TreballadorTest.java
@@ -1,6 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-class TreballadorTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TreballadorTest {
 
 @Test
 public void testNomTreballadorIncorrecte() {
@@ -9,7 +10,9 @@ public void testNomTreballadorIncorrecte() {
 // Noteu que per als objectes individuals de la classe Treballador i
 // Exception podem triar els noms que ens done la gana,
 // no cal fer-los coincidir amb els de les classes
+
 Treballador treballadorMeu = new Treballador();
+
 // Codi per a provocar excepcions
 Exception excepcioMeva = assertThrows(Exception.class, () ->
 treballadorMeu.setNom(""));
@@ -22,24 +25,17 @@ excepcioMeva.getMessage());
 public void testNomTreballadorCorrecte() {
     String nomTest = "Montsià";
     Treballador treballador = new Treballador();
-    // Assignem un nom, com el mètode setNom pot produir excepcions ha d'anar en un
-    // bloc try catch
-    // A diferencia del cas anterior, aquí no estem provocant l'excepció
-    // a propòsit,
-    // sino que és el tractament
-    // habitual de les excepcions, ja ho veureu en M3
+    // Assignem un nom, com el mètode setNom pot produir excepcions ha d'anar en un bloc try catch
+    // A diferencia del cas anterior, aquí no estem provocant l'excepció a propòsit,
+    // sino que és el tractament habitual de les excepcions, ja ho veureu en M3
     try {
     treballador.setNom(nomTest);
     } catch (Exception e) {
-    // Si es produeix una excepcio, el bloc catch la captura i
-    // mostra per pantalla,
-    // així el programa no peta
+    // Si es produeix una excepcio, el bloc catch la captura i mostra per pantalla, així el programa no peta
     System.out.println(e.getMessage());
     }
     ;
-    // L'assercio comprova que el nom és correcte i en cas contrari
-    // mostraria el
-    // missatge d'error
+    // L'assercio comprova que el nom és correcte i en cas contrari mostraria el missatge d'error
     assertEquals(nomTest, treballador.getNom(), "Els noms han de coincidir!!!");
     }
 
@@ -47,20 +43,17 @@ public void testNomTreballadorCorrecte() {
     public void testNomina() {
     float nomina = 2300;
     Treballador treballador = new Treballador();
-    // Assignem una nomina, com aquest mètode no provoca excepcions no
-    //necessitem el
-    // bloc try-catch
+    // Assignem una nomina, com aquest mètode no provoca excepcions no necessitem el bloc try-catch
     treballador.setNomina(nomina);
-    /*
-    * L'asserció comprova que la nomina és correcta i en cas contrari
-    * mostra missatge d'error
-    */
 
-    assertEquals(nomina, treballador.getNomina(), "Els dos valors de la nomina han de coincidir!!!");
+    //L'asserció comprova que la nomina és correcta i en cas contrari mostra missatge d'error
+    
+    assertEquals (nomina, treballador.getNomina(), "Els dos valors de la nomina han de coincidir!!!");
     }
+
     @Test
     public void testHoresExtres() {
-    /   // ACABEU-LO
+    /// ACABEU-LO
     }
     @Test
     public void testTipusTreballadorIncorrecte() {
